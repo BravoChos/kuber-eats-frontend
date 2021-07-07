6bcc707aa28894de57d3699673903cf932c8f462
Song Cho <iamsongcho@gmail.com>
2021-07-08 00:44:21 +0900

    Login Tests done

--- src/pages/__test__/404.spec.tsx
+++ src/pages/__test__/404.spec.tsx
@@ -1,10 +1,18 @@
-import { render, waitFor } from "../../test-utils";
+import { render, waitFor } from "@testing-library/react";
 import React from "react";
+import { HelmetProvider } from "react-helmet-async";
 import { NotFound } from "../404";
+import { BrowserRouter as Router } from "react-router-dom";
 
 describe("<NotFound />", () => {
   it("renders OK", async () => {
-    render(<NotFound />);
+    render(
+      <HelmetProvider>
+        <Router>
+          <NotFound />
+        </Router>
+      </HelmetProvider>
+    );
     await waitFor(() => {
       expect(document.title).toBe("Not Found | Nuber Eats");
     });
