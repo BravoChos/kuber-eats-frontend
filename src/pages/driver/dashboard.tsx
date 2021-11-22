010b043ac3808987dbef8f140715ec59839fb1c0
Song Cho <iamsongcho@gmail.com>
2021-11-22 14:37:36 +0900

    add google map react library

---
+++ src/pages/driver/dashboard.tsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from "react";
+import GoogleMapReact from "google-map-react";
+
+export const Dashboard = () => {
+  return (
+    <div>
+      <div
+        className="overflow-hidden"
+        style={{ width: window.innerWidth, height: "95vh" }}
+      >
+        <GoogleMapReact
+          defaultZoom={20}
+          defaultCenter={{
+            lat: 59.95,
+            lng: 30.33,
+          }}
+          bootstrapURLKeys={{ key: "AIzaSyBAAHGJkPweUpPkqNYdLCSkRickcThthlk" }} // not secured
+        ></GoogleMapReact>
+      </div>
+    </div>
+  );
+};
