9a6166bb5ac7ec7c274dd565c1ba280ee27d4713
Song Cho <iamsongcho@gmail.com>
2021-11-26 00:00:01 +0900

    add a car component

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -8,10 +8,17 @@ interface ICoords {
 
 export const Dashboard = () => {
   const [driverCoords, setDriverCoords] = useState<ICoords>({ lng: 0, lat: 0 });
+  const [map, setMap] = useState<any>();
+  const [maps, setMaps] = useState<any>();
   // @ts-ignore
   const onSucces = ({ coords: { latitude, longitude } }: Position) => {
     setDriverCoords({ lat: latitude, lng: longitude });
   };
+  useEffect(() => {
+    if (map && maps) {
+      map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
+    }
+  }, [driverCoords.lat, driverCoords.lng]);
   // @ts-ignore
   const onError = (error: PositionError) => {
     console.log(error);
@@ -23,24 +30,35 @@ export const Dashboard = () => {
   }, []);
   const onApiLoaded = ({ map, maps }: { map: any; maps: any }) => {
     map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
+    setMap(map);
+    setMaps(maps);
   };
   return (
     <div>
       <div
         className="overflow-hidden"
-        style={{ width: window.innerWidth, height: "91vh" }}
+        style={{ width: window.innerWidth, height: "50vh" }}
       >
         <GoogleMapReact
           yesIWantToUseGoogleMapApiInternals
           onGoogleApiLoaded={onApiLoaded}
-          defaultZoom={20}
+          defaultZoom={16}
           draggable={false}
           defaultCenter={{
             lat: 36.58,
             lng: 125.95,
           }}
           bootstrapURLKeys={{ key: "AIzaSyBAAHGJkPweUpPkqNYdLCSkRickcThthlk" }} // not secured
-        ></GoogleMapReact>
+        >
+          <div
+            // @ts-ignore
+            lat={driverCoords.lat}
+            lng={driverCoords.lng}
+            className="text-lg"
+          >
+            🚖
+          </div>
+        </GoogleMapReact>
       </div>
     </div>
   );
